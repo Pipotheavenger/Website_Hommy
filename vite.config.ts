388b581b2b93546abec933c2fe35ca92d364d676
Pipotheavenger <sebastianfelipevalencia@hotmail.com>
2026-03-16 12:55:32 -0500

    domain update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/Website_Hommy/',
+  base: '/web.hommy.app/',
   build: {
     outDir: 'docs',
     rollupOptions: {
